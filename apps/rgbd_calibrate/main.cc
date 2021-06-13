8808bdcd4a98916719b3f78ec3f9a8efbdfa7494
Toki Migimatsu <toki.migimatsu@gmail.com>
2021-06-12 20:42:22 -0700

    Tighten loop in calibration app

--- apps/rgbd_calibrate/main.cc
+++ apps/rgbd_calibrate/main.cc
@@ -54,13 +54,18 @@ struct Args : ctrl_utils::Args {
   std::string key_pos =
       Kwarg<std::string>("pos-key", "franka_panda::sensor::pos",
                          "Redis key for the robot's end-effector position.");
+
+  std::string key_prefix_camera = Kwarg<std::string>(
+      "camera-key-prefix", "kinect2::camera_0::",
+      "Redis key prefix for the camera's pose. The quaternion will be set to "
+      "<camera-key-prefix>ori and the position to <camera-key-prefix>pos.");
 };
 
 struct CameraRobotState {
   cv::Mat img_color;
   cv::Mat img_depth;
-  cv::Point3d pos_ee;
-  cv::Point2d point_img;
+  std::vector<cv::Point3d> points_ee;
+  std::vector<cv::Point2d> points_img;
   std::binary_semaphore ownership = std::binary_semaphore(false);
   std::binary_semaphore data_ready = std::binary_semaphore(false);
 };
@@ -107,56 +112,90 @@ int main(int argc, char* argv[]) {
 
   // Preallocate registered images.
   CameraRobotState state;
+  state.img_color = cv::Mat(camera->color_width(), camera->color_height(),
+                            camera->color_channel());
   state.img_depth = cv::Mat(camera->color_width(), camera->color_height(),
                             camera->depth_channel());
-  cv::Mat img_bgr_flipped = cv::Mat(
-      camera->color_width(), camera->color_height(), camera->color_channel());
+  cv::Mat img_depth_registered(camera->color_width(), camera->color_height(),
+                               camera->depth_channel());
 
-  std::thread thread_calibrate([camera, &state]() {
+  // Start calibration thread.
+  std::thread thread_calibrate([&args, camera, &redis, &state]() {
     std::vector<cv::Point3d> points_ee;
     std::vector<cv::Point2d> points_img;
 
     while (g_runloop) {
       // Get data when available.
       state.data_ready.acquire();
-      points_ee.push_back(state.pos_ee);
-      points_img.push_back(state.point_img);
+      points_ee.insert(points_ee.end(),
+                       std::make_move_iterator(state.points_ee.begin()),
+                       std::make_move_iterator(state.points_ee.end()));
+      state.points_ee.clear();
+      points_img.insert(points_img.end(),
+                        std::make_move_iterator(state.points_img.begin()),
+                        std::make_move_iterator(state.points_img.end()));
+      state.points_img.clear();
       state.ownership.release();
 
       if (points_ee.size() < 3) continue;
 
       // Optimize.
-      cv::Mat rvec, tvec;
+      cv::Vec3d rvec, tvec;
       // TODO: Fix intrinsic matrix.
       cv::solvePnP(points_ee, points_img, camera->color_intrinsic_matrix(),
                    camera->color_distortion_coeffs(), rvec, tvec);
 
+      // Convert position.
+      const Eigen::Translation3d p_world_to_camera(tvec[0], tvec[1], tvec[2]);
+
+      // Convert angle.
+      const Eigen::Vector3d aa_vec(rvec[0], rvec[1], rvec[2]);
+      const double angle = aa_vec.norm();
+      const Eigen::Vector3d axis =
+          angle != 0 ? (aa_vec / angle).eval() : Eigen::Vector3d::Identity();
+      const Eigen::AngleAxisd aa_world_to_camera(angle, axis);
+
+      // Invert transformation.
+      const Eigen::Isometry3d T_world_to_camera =
+          p_world_to_camera * aa_world_to_camera;
+      const Eigen::Isometry3d T_camera_to_world = T_world_to_camera.inverse();
+
+      // Extract position and quaternion.
+      const Eigen::Vector3d pos = T_camera_to_world.translation();
+      const Eigen::Quaterniond quat(T_camera_to_world.linear());
+
+      // Send camera pose to Redis.
+      redis.set(args->key_prefix_camera + "pos", pos);
+      redis.set(args->key_prefix_camera + "ori", quat);
+      redis.commit();
+
       std::cout << "ee positions:" << std::endl;
       for (const cv::Point3d& point : points_ee) {
-      std::cout << point << std::endl;
+        std::cout << point << std::endl;
       }
       std::cout << "image points:" << std::endl;
       for (const cv::Point2d& point : points_img) {
-      std::cout << point << std::endl;
+        std::cout << point << std::endl;
       }
       std::cout << "rvec: " << rvec << std::endl;
       std::cout << "tvec: " << tvec << std::endl;
     }
   });
-  std::vector<cv::Point2d> image_points;
 
   // Create image window.
   cv::namedWindow(camera->name());
   cv::setMouseCallback(
       camera->name(),
       [](int event, int x, int y, int flags, void* userdata) {
+        // Trigger on left button up.
         if (event != cv::EVENT_LBUTTONUP) return;
 
         CameraRobotState& state =
             *reinterpret_cast<CameraRobotState*>(userdata);
+
+        // Send click pose.
         state.ownership.acquire();
-        state.point_img.x = x;
-        state.point_img.y = y;
+        state.points_img.emplace_back(x, y);
         state.data_ready.release();
       },
       &state);
@@ -166,21 +205,18 @@ int main(int argc, char* argv[]) {
   while (g_runloop) {
     timer.Sleep();
 
-    if (!state.ownership.try_acquire()) continue;
-
-    // Get end-effector position from Redis.
+    // Request end-effector position from Redis.
     std::future<Eigen::Vector3d> fut_pos_ee =
         redis.get<Eigen::Vector3d>(args->key_pos);
     redis.commit();
 
     // Get color and depth images.
-    state.img_color = camera->color_image();
+    cv::Mat img_color = camera->color_image();
     cv::Mat img_depth = camera->depth_image();
 
     // Register depth image to color.
     if (kinect2 != nullptr) {
-      kinect2->RegisterDepthToColor(img_depth, state.img_color,
-                                    state.img_depth);
+      kinect2->RegisterDepthToColor(img_depth, img_color, img_depth_registered);
     } else {
       throw std::runtime_error("Not implemented yet.");
     }
@@ -188,8 +224,8 @@ int main(int argc, char* argv[]) {
     // Zero out unseen depth pixels.
     for (int i = 0; i < camera->color_height(); i++) {
       for (int j = 0; j < camera->color_width(); j++) {
-        if (state.img_depth.at<float>(i, j) <= 0) {
-          cv::Vec3b pixel = state.img_color.at<cv::Vec3b>(i, j);
+        if (img_depth.at<float>(i, j) <= 0) {
+          cv::Vec3b pixel = img_color.at<cv::Vec3b>(i, j);
           pixel = 0;
         }
       }
@@ -197,15 +233,20 @@ int main(int argc, char* argv[]) {
 
     // Get end-effector position.
     const Eigen::Vector3d pos_ee = fut_pos_ee.get();
-    state.pos_ee.x = pos_ee(0);
-    state.pos_ee.y = pos_ee(1);
-    state.pos_ee.z = pos_ee(2);
 
-    state.ownership.release();
+    // If calibration isn't ready, try next cycle.
+    if (!state.ownership.try_acquire()) continue;
 
-    // Flip image about Y-axis before showing.
-    cv::flip(state.img_color, img_bgr_flipped, 1);
+    // Update state.
+    state.points_ee.emplace_back(pos_ee(0), pos_ee(1), pos_ee(2));
+    std::swap(img_color, state.img_color);
+    std::swap(img_depth, state.img_depth);
+
+    // Show image.
     cv::imshow(camera->name(), state.img_color);
+
+    // Allow click to process with updated image..
+    state.ownership.release();
   }
 
   return 0;
